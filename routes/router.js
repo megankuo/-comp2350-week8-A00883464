3100b4d5d1926a0985988fa33d969ed81ddcb37c
Megan Kuo <megan.kuo@live.com>
2021-03-10 21:53:56 -0800

    added router to /addUser

--- routes/router.js
+++ routes/router.js
@@ -4,33 +4,34 @@ const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
 
 router.get('/', (req, res) => {
-	console.log("page hit");
-	database.getConnection(function (err, dbConnection) {
-		if (err) {
-			res.render('error', {message: 'Error connecting to MySQL'});
-			console.log("Error connecting to mysql");
-			console.log(err);
-		}
-		else {
-			
-			dbModel.getAllUsers((err, result) => {
-				if (err) {
-					res.render('error', {message: 'Error reading from MySQL'});
-					console.log("Error reading from mysql");
-					console.log(err);
-				}
-				else { //success
-					res.render('index', {allUsers: result});
+  console.log('page hit');
+  database.getConnection(function (err, dbConnection) {
+    if (err) {
+      res.render('error', { message: 'Error connecting to MySQL' });
+      console.log('Error connecting to mysql');
+      console.log(err);
+    } else {
+      dbModel.getAllUsers((err, result) => {
+        if (err) {
+          res.render('error', { message: 'Error reading from MySQL' });
+          console.log('Error reading from mysql');
+          console.log(err);
+        } else {
+          //success
+          res.render('index', { allUsers: result });
 
-					//Output the results of the query to the Heroku Logs
-					console.log(result);
-				}
-			});
-			dbConnection.release();
-		}
-	});
-
+          //Output the results of the query to the Heroku Logs
+          console.log(result);
+        }
+      });
+      dbConnection.release();
+    }
+  });
 });
 
+router.post('/addUser', (req, res) => {
+  console.log('form submit');
+  console.log(req.body);
+});
 
 module.exports = router;
